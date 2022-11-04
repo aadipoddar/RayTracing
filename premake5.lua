bc514eb9ec7a003ea5162d84f6db4638bace4f0f
Aadi Poddar <aadipoddarmail@gmail.com>
2022-11-04 18:39:47 +0530

    Materials and Physically Based Rendering

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@
 workspace "RayTracing"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "RayTracing"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
